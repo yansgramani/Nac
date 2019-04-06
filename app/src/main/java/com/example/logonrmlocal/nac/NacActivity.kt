be8cb465c2773557fa33a88e843f1d347d4c4e2f
Logon Aluno <logonrmlocal@fiap.com.br>
2019-04-05 22:06:16 -0300

    Initial commit

---
+++ app/src/main/java/com/example/logonrmlocal/nac/NacActivity.kt
@@ -0,0 +1,13 @@
+package com.example.logonrmlocal.nac
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class NacActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_nac)
+    }
+
+}
